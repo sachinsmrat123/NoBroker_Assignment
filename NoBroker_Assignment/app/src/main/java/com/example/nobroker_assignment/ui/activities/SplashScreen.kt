124c761db2e09437243bcf8a85cce39986247f15
sachinsmrat123 <36136410+sachinsmrat123@users.noreply.github.com>
2021-07-25 04:09:28 +0530

    splash screen added

--- NoBroker_Assignment/app/src/main/java/com/example/nobroker_assignment/ui/activities/SplashScreen.kt
+++ NoBroker_Assignment/app/src/main/java/com/example/nobroker_assignment/ui/activities/SplashScreen.kt
@@ -1,13 +1,34 @@
 package com.example.nobroker_assignment.ui.activities
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.nobroker_assignment.R
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 
 class SplashScreen : AppCompatActivity() {
+
+    lateinit var frombottom: Animation
+    lateinit var froTop: Animation
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        frombottom = AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
+        cdBtn.setAnimation(frombottom)
+        froTop = AnimationUtils.loadAnimation(this, R.anim.top_animation)
+        voteimage.animation = froTop
+        cdBtn.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this@SplashScreen, MainActivity::class.java)
+            startActivity(intent)
+        })
     }
 }
